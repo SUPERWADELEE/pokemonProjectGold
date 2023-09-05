718cd4f317f7cf26081cb8d2931bc38ec291bae4
李威德 <liweide@liweidedeMacBook-Air.local>
2023-09-05 16:14:06 +0800

    解決登出後還看得到上一個使用者的資訊問題

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,30 +1,29 @@
 function logout() {
-    // 調用API的登出端點
-    const token = localStorage.getItem('jwtToken');
-    fetch('http://localhost:8000/api/Auth/logout', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer ' + token ,
-          'Accept': 'application/json',// 這裡添加token
-        }
-      })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Logout failed');
-        }
-        return response.json();
-      })
-      .then(data => {
-        console.log('Logged out successfully');
-        showLoginPage()
-      })
-      .catch(error => {
-        console.error('Logout error:', error);
-      });
-    localStorage.removeItem('jwtToken');
-
-
-
-
-  }
+  const token = localStorage.getItem('jwtToken');
+  fetch('http://localhost:8000/api/Auth/logout', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token ,
+        'Accept': 'application/json'
+      }
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Logout failed');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('Logged out successfully');
+      localStorage.removeItem('jwtToken');  // 确保在确认登出成功后删除token
+      showLoginPage();
+      
+      // 强制重新加载页面  
+      window.location.reload(true);
+      
+    })
+    .catch(error => {
+      console.error('Logout error:', error);
+    });
+}
